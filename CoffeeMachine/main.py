cf648de68b1a4fa9bb75e7ade0902653b7719019
An Nguyen <nguyen.an4@northeastern.edu>
2024-09-10 17:37:38 -0700

    Adding more code to CoffeeMachine project

---
+++ CoffeeMachine/main.py
@@ -0,0 +1,57 @@
+from art import logo
+
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+print(logo)
+
+active_state = True
+
+def print_report():
+    print(resources)
+
+def enough_supply(drink):
+    water = resources["water"]
+    milk = resources["milk"]
+    coffee = resources["coffee"]
+
+    if water < MENU[drink]["ingredients"]["water"]:
+        print("Sorry there is not enough water")
+
+def process_coin():
+
+
+def check_transaction():
+
+
+
+    coffee = input("What would you like customer? (espresso/latte/cappuccino)\n")
