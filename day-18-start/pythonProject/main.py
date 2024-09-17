9fd51ca54629c2d6104806e35b6e91c3fa876963
An Nguyen <nguyen.an4@northeastern.edu>
2024-09-17 04:46:33 -0700

    more changes to turtle challenges

--- day-18-start/pythonProject/main.py
+++ day-18-start/pythonProject/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import turtle as t
 import random
 
 timmy_the_turtle = Turtle()
@@ -42,8 +43,10 @@ timmy_the_turtle.color("turquoise")
 # turtle can draw any n sided shape with l length. Specify the parameters.
 # Within the for loop turtle can stack drawn shape. Each shape a random color chosen
 
-# color = ["DarkGreen","burlywood4","CadetBlue","cornsilk","DarkOrchid4",
+#color = ["DarkGreen","burlywood4","CadetBlue","cornsilk","DarkOrchid4",
 #          "hotpink", "navy","tomato4", "SeaGreen4", "VioletRed4"]
+
+# direction = [0,90,180,270]
 #
 # def draw_shape(n_side, l_side, color):
 #     for x in range(n_side):
@@ -56,22 +59,53 @@ timmy_the_turtle.color("turquoise")
 #     c = random.choice(color)
 #     draw_shape(x,100, c)
 
+#--------------------------Random Turtle Color Walk-------------------------
 
-screen = Screen()
-screen.exitonclick()
+# turtle random walking in any 4 directions by a fixed distance. Each walk
+# will be followed by a random color. Thicker pen size.
 
+# timmy_the_turtle = t.Turtle()
+# t.colormode(255)
+#
+# timmy_the_turtle.pensize(15)
+# timmy_the_turtle.speed("fastest")
+#
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     random_color = (r,g,b)
+#     return random_color
+#
+# def dir_turn():
+#     timmy_the_turtle.setheading(random.choice(direction))
+#     timmy_the_turtle.forward(30)
+#
+# for x in range(200):
+#     timmy_the_turtle.color(random_color())
+#     dir_turn()
+#
+# screen = Screen()
+# screen.exitonclick()
 
+#-----------------------------Spirograph Turtle----------------------------
 
-
-
-
-
-
-
-
-
-
-
+# timmy_the_turtle = t.Turtle()
+# t.colormode(255)
+# timmy_the_turtle.speed("fastest")
+# timmy_the_turtle.circle(150)
+#
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0, 255)
+#     b = random.randint(0, 255)
+#     random_color = (r,g,b)
+#     return random_color
+#
+# for x in range(0,360,5):
+#     timmy_the_turtle.color(random_color())
+#     timmy_the_turtle.setheading(x)
+#     timmy_the_turtle.circle(150)
 
 
 screen = Screen()
